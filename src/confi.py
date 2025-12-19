dc91df05ab38d5292c32ff8d8604843d13263617
Tushar Sharma <sharmatushar2505@gmail.com>
2025-12-19 15:31:31 +0530

    confi

---
+++ src/confi.py
@@ -0,0 +1,14 @@
+# src/config.py
+
+CLASSES = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'U', 'V', 'W', 'X', 'Y'
+]
+
+TARGET_SAMPLES = 400
+DATASET_PATH = "data/asl_landmark_dataset.csv"
+
+SAVE_INTERVAL = 0.2  # seconds
+CAMERA_INDEX = 0
+
